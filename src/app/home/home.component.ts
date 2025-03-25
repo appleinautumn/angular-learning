87aadf45f2540c946be188012db62c5ff62e9ef2
Firdaus Adi Putra <mail@firdaus.org>
2025-03-25 18:31:13 +0700

    Filter list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,14 +11,14 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
 
     <section class="results">
       <app-housing-location
-        *ngFor="let l of housingLocationList"
+        *ngFor="let l of filteredLocationList"
         [housingLocation]="l"
       ></app-housing-location>
     </section>
@@ -27,13 +27,25 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   async ngOnInit() {
     try {
       this.housingLocationList = await this.housingService.getAllHousingLocations();
+      this.filteredLocationList = this.housingLocationList;
     } catch (error) {
       console.error('Failed to fetch housing locations:', error);
     }
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
